0481af9d7704572d8fd3abd112887c39dd71cfa4
U-NickGiglia\Nick <Nick@NickGiglia.(none)>
2011-08-10 17:46:48 -0400

    Initial Scott Mayfield Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mayfield::Application.config.secret_token = 'ed50cc5e1a643f4ed5174b2ab867f34d81c6f20f99bf36ab944a0138c495d391f62aa053efc110f767ce5d28fef2defa74877bd0739202fa4ee3632eb7c694d3'
